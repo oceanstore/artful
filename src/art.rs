0f66be07c491c019421369a2c16133a581cda008
neo <jackson.cloudnative@gmail.com>
2022-08-29 18:22:29 +0800

    add docs for user-visiable methods and Art struct.
    Signed-off-by: neo <jackson.cloudnative@gmail.com>

--- src/art.rs
+++ src/art.rs
@@ -10,6 +10,33 @@ use std::cmp::min;
 use std::mem::take;
 use std::ops::Deref;
 
+/// Art is an **adaptive radix tree**, which are also known as radix trees and
+/// prefix trees.
+///
+/// Radix tress consist of two types of nodes: inner and leaf nodes. Inner node map
+/// partial keys to other nodes and leaf node store key-value pair.
+///
+/// For large keys, comparisons actually take $O(k)$ time. the complexity of
+/// search trees is $O(k\log_n)$ and the art complexity of $O(k)$.
+///
+/// Art key idea that achieves both space and time efficiency is to adaptively use different node size with
+/// the same, relatively large space, but with different fanout.
+///
+/// Specifically, Art uses four node types of dynamically adaptive representation of inner nodes to
+/// achieve space and time efficiency.
+/// 1. Node4, which store up to 4 child pointers and uses an array of length 4 for keys. The keys
+///    and pointers are stored at corresponding positions and the keys are sorted. In the
+///    future,Art will no maintenance order and use simd.
+/// 2. Node16, Which storing 5 and 16 child pointers.  Like the Node4, the keys and pointers
+///    are stored in separate arrays at corresponding positions, but both arrays have space
+///    for 16 entries. **A key use SIMD instructions to parallel comparisons on modern hardware.**
+/// 3. Node48, which node storing 17 and 48 child pointers. Unlike Node4 and Node16, Node48 uses
+///    an array of length 256 for keys to index child. because seaching becomes expensive.
+/// 4. Node256, which largest node type is simply an array of 256 pointers.
+///
+///
+/// See [The Adaptive Radix Tree: ARTful indexing for Main-Memory Databases](https://db.in.tum.de/~leis/papers/ART.pdf)
+/// for more information.
 pub struct Art<K, V, const MAX_PARTIAL_LEN: usize = 8>
 where
     K: ArtKey,
@@ -26,10 +53,17 @@ impl<K: ArtKey, V: Default, const MAX_PARTIAL_LEN: usize> Art<K, V, MAX_PARTIAL_
         }
     }
 
+    /// Returns a reference to the value corresponding to the key.
+    /// The key may be any borrowed form of the map’s key type and must be implementation `ArtKey` trait.
     pub fn get(&self, key: &K) -> Option<&V> {
         ArtNode::get(&self.root, key.get_bytes(), 0)
     }
 
+    /// Inserts a key-value pair into the map.
+    ///
+    /// If the map did not have this key present, None is returned.
+    ///
+    /// If the map did have this key present, the value is updated, and the old value is returned. The key is not updated, though;
     pub fn insert(&mut self, key: K, val: V) -> Option<V> {
         if let Some(old_val) = ArtNode::insert(&mut self.root, key, val, 0) {
             return Some(old_val);
@@ -39,12 +73,19 @@ impl<K: ArtKey, V: Default, const MAX_PARTIAL_LEN: usize> Art<K, V, MAX_PARTIAL_
         None
     }
 
+    /// Removes a key from the map, returning the value at the key if the key was previously in the map.
+    ///
+    /// The key may be any borrowed form of the map’s key type and must be implementation `ArtKey` trait.
     pub fn remove(&mut self, key: &K) -> Option<V> {
         let val = ArtNode::remove(&mut self.root, key.get_bytes(), 0)?;
         self.size -= 1;
         Some(val)
     }
 
+    /// Returns the size of key-value pairs in Art.
+    ///
+    /// For insertion, size is incremented only when the key is different. For deletion size is decremented
+    /// only when the key exists.
     #[inline(always)]
     pub fn size(&self) -> usize {
         self.size
