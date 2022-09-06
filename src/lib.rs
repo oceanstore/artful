e6c65064ee079ddae6816e0ce3f834c793d04076
Oracle <yuanchang.xu@outlook.com>
2022-09-06 13:23:53 +0800

    improve: support more built-in primitive types to implementation ArtKey
    Signed-off-by: Oracle <yuanchang.xu@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,30 @@ pub trait ArtKey: Default {
     fn get_mut_bytes(&mut self) -> &mut [u8];
 }
 
+impl ArtKey for i8 {
+    fn get_bytes(&self) -> &[u8] {
+        let ptr = self as *const i8 as *const u8;
+        unsafe { std::slice::from_raw_parts(ptr, 1) }
+    }
+
+    fn get_mut_bytes(&mut self) -> &mut [u8] {
+        let ptr = self as *mut i8 as *mut u8;
+        unsafe { std::slice::from_raw_parts_mut(ptr, 1) }
+    }
+}
+
+impl ArtKey for i16 {
+    fn get_bytes(&self) -> &[u8] {
+        let ptr = self as *const i16 as *const u8;
+        unsafe { std::slice::from_raw_parts(ptr, 2) }
+    }
+
+    fn get_mut_bytes(&mut self) -> &mut [u8] {
+        let ptr = self as *mut i16 as *mut u8;
+        unsafe { std::slice::from_raw_parts_mut(ptr, 2) }
+    }
+}
+
 impl ArtKey for i32 {
     fn get_bytes(&self) -> &[u8] {
         let ptr = self as *const i32 as *const u8;
@@ -40,6 +64,78 @@ impl ArtKey for i64 {
 
     fn get_mut_bytes(&mut self) -> &mut [u8] {
         let ptr = self as *mut i64 as *mut u8;
+        unsafe { std::slice::from_raw_parts_mut(ptr, 8) }
+    }
+}
+
+impl ArtKey for u8 {
+    fn get_bytes(&self) -> &[u8] {
+        let ptr = self as *const u8;
+        unsafe { std::slice::from_raw_parts(ptr, 1) }
+    }
+
+    fn get_mut_bytes(&mut self) -> &mut [u8] {
+        let ptr = self as *mut u8;
+        unsafe { std::slice::from_raw_parts_mut(ptr, 1) }
+    }
+}
+
+impl ArtKey for u16 {
+    fn get_bytes(&self) -> &[u8] {
+        let ptr = self as *const u16 as *const u8;
+        unsafe { std::slice::from_raw_parts(ptr, 2) }
+    }
+
+    fn get_mut_bytes(&mut self) -> &mut [u8] {
+        let ptr = self as *mut u16 as *mut u8;
+        unsafe { std::slice::from_raw_parts_mut(ptr, 2) }
+    }
+}
+
+impl ArtKey for u32 {
+    fn get_bytes(&self) -> &[u8] {
+        let ptr = self as *const u32 as *const u8;
+        unsafe { std::slice::from_raw_parts(ptr, 4) }
+    }
+
+    fn get_mut_bytes(&mut self) -> &mut [u8] {
+        let ptr = self as *mut u32 as *mut u8;
+        unsafe { std::slice::from_raw_parts_mut(ptr, 4) }
+    }
+}
+
+impl ArtKey for u64 {
+    fn get_bytes(&self) -> &[u8] {
+        let ptr = self as *const u64 as *const u8;
+        unsafe { std::slice::from_raw_parts(ptr, 8) }
+    }
+
+    fn get_mut_bytes(&mut self) -> &mut [u8] {
+        let ptr = self as *mut u64 as *mut u8;
+        unsafe { std::slice::from_raw_parts_mut(ptr, 8) }
+    }
+}
+
+impl ArtKey for f32 {
+    fn get_bytes(&self) -> &[u8] {
+        let ptr = self as *const f32 as *const u8;
+        unsafe { std::slice::from_raw_parts(ptr, 4) }
+    }
+
+    fn get_mut_bytes(&mut self) -> &mut [u8] {
+        let ptr = self as *mut f32 as *mut u8;
+        unsafe { std::slice::from_raw_parts_mut(ptr, 4) }
+    }
+}
+
+impl ArtKey for f64 {
+    fn get_bytes(&self) -> &[u8] {
+        let ptr = self as *const f64 as *const u8;
+        unsafe { std::slice::from_raw_parts(ptr, 8) }
+    }
+
+    fn get_mut_bytes(&mut self) -> &mut [u8] {
+        let ptr = self as *mut f64 as *mut u8;
         unsafe { std::slice::from_raw_parts_mut(ptr, 8) }
     }
 }
